8108a490d39ca3b7f316c9875aaf9503a559ce4e
pyodolski <pjwp0928w@gmail.com>
2025-07-17 10:12:42 +0900

    프로젝트 생성 및 postgreSQL 연결 성공

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from config import Config
+from models import db
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+# 최초 실행 시 테이블 생성 models.py 기반 지우지 말 것!
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return 'Flask + PostgreSQL 연결 완료!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://pyodolski:comep1522w@localhost/senior_job_care'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
