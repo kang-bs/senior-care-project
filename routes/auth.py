eb7ade438a3328d5469085b7d2da444de9bf12c7
pyodolski <pjwp0928w@gmail.com>
2025-07-18 11:44:48 +0900

    일반 로그인 추가

--- routes/auth.py
+++ routes/auth.py
@@ -4,6 +4,7 @@ from flask_dance.contrib.google import google
 from models import db, User
 import requests
 from config import Config
+import bcrypt
 
 # 인증 관련 라우트를 담당하는 블루프린트 생성
 auth_bp = Blueprint("auth", __name__)
@@ -46,13 +47,6 @@ def google_login_callback():
     # 로그인 처리 후 프로필 페이지로 이동
     login_user(user)
     return redirect(url_for("auth.profile"))
-
-# 로그인한 사용자의 프로필 페이지
-@auth_bp.route("/profile")
-@login_required
-def profile():
-    return render_template("profile.html", user=current_user)
-
 
 # 카카오 OAuth 로그인 후 콜백 처리
 @auth_bp.route("/kakao_login_callback")
@@ -116,6 +110,73 @@ def kakao_login_callback():
     # 로그인 처리 후 프로필 페이지로 이동
     login_user(user)
     return redirect(url_for("auth.profile"))
+
+# 로그인한 사용자의 프로필 페이지
+@auth_bp.route("/profile")
+@login_required
+def profile():
+    return render_template("profile.html", user=current_user)
+import bcrypt
+from flask import request, flash
+
+# 회원가입 라우트
+@auth_bp.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        nickname = request.form["nickname"]
+        name = request.form.get("name")
+        gender = request.form.get("gender")
+        birth_date = request.form.get("birth_date")
+
+        if password != confirm_password:
+            flash("비밀번호가 일치하지 않습니다.")
+            return redirect(url_for("auth.register"))
+
+        if User.query.filter_by(username=username).first():
+            flash("이미 존재하는 사용자입니다.")
+            return redirect(url_for("auth.register"))
+
+        hashed_pw = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+        user = User(
+            username=username,
+            password=hashed_pw.decode("utf-8"),
+            nickname=nickname,
+            name=name,
+            gender=gender,
+            birth_date=birth_date if birth_date else None,
+            user_type=0,
+            social_type=None,
+            social_id=None
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("회원가입 완료! 로그인 해주세요.")
+        return redirect(url_for("auth.home"))
+
+    return render_template("register.html")
+
+
+# 로그인 라우트
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(username=username).first()
+        if not user or not bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
+            flash("로그인 실패. 아이디 또는 비밀번호를 확인하세요.")
+            return redirect(url_for("auth.login"))
+
+        login_user(user)
+        return redirect(url_for("auth.profile"))
+
+    return render_template("login.html")
+
 
 
 # 로그아웃 처리
